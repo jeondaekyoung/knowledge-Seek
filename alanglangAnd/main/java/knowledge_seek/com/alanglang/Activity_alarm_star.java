b8ec2538fafff829e622b51481480f2f9718c1a1
seo_cjswo <seo_cjswo@6880a085-1421-0510-aa6f-a040f8e32b20>
2015-10-22 06:32:23 +0000

    스타알람

--- alanglangAnd/main/java/knowledge_seek/com/alanglang/Activity_alarm_star.java
+++ alanglangAnd/main/java/knowledge_seek/com/alanglang/Activity_alarm_star.java
@@ -1,6 +1,7 @@
 package knowledge_seek.com.alanglang;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -16,6 +17,8 @@ import knowledge_seek.com.receiver.StaticWakeLock;
 public class Activity_alarm_star extends Activity {
 
     private Alarm alarm;
+    private MediaPlayer mediaPlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +38,19 @@ public class Activity_alarm_star extends Activity {
                 finish();
             }
         });
+
+        try {
+            playAudio();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
     @Override
     protected void onResume() {
         super.onResume();
+
     }
 
     @Override
@@ -52,5 +62,24 @@ public class Activity_alarm_star extends Activity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        killMediaPlayer();
+    }
+
+    private void playAudio() throws Exception{
+        killMediaPlayer();
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.kyc_sp1);
+        mediaPlayer.start();
+    }
+
+    private void killMediaPlayer(){
+        if(mediaPlayer != null){
+            try{
+                mediaPlayer.release();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
